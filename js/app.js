6f71fa403d53ea6d2d0a8790764ec05a71e7689d
ArvidQuarshie <arvidquarshie@gmail.com>
2016-05-28 00:10:46 +0300

    added menu

--- js/app.js
+++ js/app.js
@@ -1,1 +1,6 @@
-$(document).foundation()
+$(document).ready(
+  $(".menu__item").click('.menu__link',function(e){
+    $(".menu__item").removeClass("menu__item--current");
+    $(this).addClass("menu__item--current");
+  })
+);
