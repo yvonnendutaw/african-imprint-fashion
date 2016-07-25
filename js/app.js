fea482771969f70e83c4282ff7f45780e596fcc2
ArvidQuarshie <arvidquarshie@gmail.com>
2016-07-25 08:53:47 +0300

    linked navigation with js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,22 @@
+$(function () {
+    $('a[href*="#"]:not([href="#"])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
+
+
 $(document).ready(
-  $(".menu__item").click('.menu__link',function(e){
-    $(".menu__item").removeClass("menu__item--current");
-    $(this).addClass("menu__item--current");
-  })
+    $(".menu__item").click('.menu__link', function (e) {
+        $(".menu__item").removeClass("menu__item--current");
+        $(this).addClass("menu__item--current");
+    })
 );
